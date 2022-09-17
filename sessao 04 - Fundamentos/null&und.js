0c9615a030fdc34ad6a288b1400bed8433109f6b
Luan Rabelo <luanrbcontato@gmail.com>
2022-09-17 10:21:45 -0300

    add new files sessao 04

---
+++ sessao 04 - Fundamentos/null&und.js
@@ -0,0 +1,26 @@
+//conceito de undefined
+//por referencia
+const a = {name: 'teste'} //constante 'a' recebe um endereco apontando para o objeto
+console.log(a)
+const b = a //recebe um endereco
+console.log(b)
+
+//primitivo, cada um tem seu proprio valor sem endereçar
+let c = 3 
+let d = 3
+console.log(c)
+console.log(d)
+console.log(c+d)
+c++
+console.log(c)
+console.log(d)
+console.log(c+d)
+
+/*
+plus
+*/
+
+let valor
+console.log("nao icializada: " + valor)
+valor = null // ausencia de valor
+console.log("ausencia de valor: " + valor)
