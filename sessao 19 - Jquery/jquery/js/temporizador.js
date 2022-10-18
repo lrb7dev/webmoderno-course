6a654a7c9ef8cf1214a09badcb0d466feb95cb89
lrb7dev <luanrb.dev@gmail.com>
2022-10-18 19:26:56 -0300

    new

--- sessao 19 - Jquery/jquery/js/temporizador.js
+++ sessao 19 - Jquery/jquery/js/temporizador.js
@@ -1,53 +1,84 @@
+//funcao protegendo '$'
 (function ($) {
-    $.fn.temporizador = function (opcoes) {
+    $.fn.temporizador = function(opcoes) {
+        //mesclar o params padrao + params na chamada da funcao
         const opcoesFinais = $.extend({
+            //msg padrao
             mensagem: 'Em breve!',
-            horario: '23:59:59'
-        }, opcoes)
-
+            //horario padrao
+            horario: '00:00:00'
+        }, opcoes) //se passar opcoes, sera sobreescrito os valores padroes acima
+        
+        //digitos do temporizador
         const horaDezena = $('<span class="digito">').html('0')
         const horaUnidade = $('<span class="digito">').html('0')
         const minutoDezena = $('<span class="digito">').html('0')
         const minutoUnidade = $('<span class="digito">').html('0')
         const segundoDezena = $('<span class="digito">').html('0')
         const segundoUnidade = $('<span class="digito">').html('0')
-
+        //separadores do temporizador
         const separadorHora = $('<span class="separador">').html(':')
         const separadorMinuto = $('<span class="separador">').html(':')
+        //mensagem
         const mensagem = $('<div class="mensagem">').html(opcoesFinais.mensagem)
 
+        //adicionar .temporizador
         $(this).addClass('temporizador')
+        //juntando no elemento
         $(this).append(horaDezena, horaUnidade, separadorHora,
             minutoDezena, minutoUnidade, separadorMinuto,
-            segundoDezena, segundoUnidade, mensagem)
+            segundoDezena, segundoUnidade,
+            mensagem)
 
+        //capturando hora:minuto:segundo -- regex
         const regex = new RegExp(/(\d\d):(\d\d):(\d\d)/)
+        //horario alvo
         const horarioAlvo = regex.exec(opcoesFinais.horario)
-        // console.log(horarioAlvo)
+        //console.log(horarioAlvo)
 
+        //definindo o temporarizador
         let temporizador = setInterval(() => {
-            const agora = new Date()
+            const agora = new Date() //date atual
             const alvo = new Date()
-            alvo.setHours(horarioAlvo[1])
-            alvo.setMinutes(horarioAlvo[2])
-            alvo.setSeconds(horarioAlvo[3])
+            alvo.setHours(horarioAlvo[1]) //index que esta as horas
+            alvo.setMinutes(horarioAlvo[2]) // index que esta os minutos
+            alvo.setSeconds(horarioAlvo[3]) //index que esta os segundos
 
+            //pegar a diferença dos dois
             const diferencaEmMili = alvo.getTime() - agora.getTime()
-            if (diferencaEmMili >= 0) {
+            //o alvo tem que ser maior que a hora agora
+            //condicao enquanto a diferença entre alvo futuro e hora agora nao é alcançada
+            if(diferencaEmMili >= 0) {
+                //diferença receba o reg como nova data com ISOString para nao pegar o time zone
                 const diferenca = regex.exec(new Date(diferencaEmMili).toISOString())
-                console.log(diferenca)
+                //testando...
+                //console.log(diferenca)
 
+                //alterando o html para mostrar a contabilizacao
+                //pegando o index[1] da diferença e nela o index[0] dela, para pegar a dezena das horas
                 horaDezena.html(diferenca[1][0])
+                //pegando o index[1] da diferença e nela o index[1] dela, para pegar a unidade das horas
                 horaUnidade.html(diferenca[1][1])
+                //pegando o index[2] da diferença e nela o index[0] dela, para pegar a dezena dos minutos
                 minutoDezena.html(diferenca[2][0])
+                //pegando o index[2] da diferença e nela o index[1] dela, para pegar a unidade dos minutos
                 minutoUnidade.html(diferenca[2][1])
+                //pegando o index[3] da diferença e nela o index[0] dela, para pegar a dezena dos segundos
                 segundoDezena.html(diferenca[3][0])
+                //pegando o index[3] da diferença e nela o index[1] dela, para pegar a unidade dos segundos
                 segundoUnidade.html(diferenca[3][1])
-            } else {
-                clearInterval(temporizador)
+
+
+
             }
+
+
         }, 1000)
-        
+
+
+
+
         return this
     }
+    
 })(jQuery)
