bbf50b38315f9ece2c80e2e6385f3e0e2cc3aac9
lrb7dev <luanrb.dev@gmail.com>
2022-10-23 14:32:37 -0300

    ajustando erros

--- sessao 22 - React/calculadora/src/components/Button.jsx
+++ sessao 22 - React/calculadora/src/components/Button.jsx
@@ -1,5 +1,38 @@
 import React from 'react'
 import './Button.css'
 
-export default props =>
-    <button className="button">{props.label}</button>
+//outra forma...
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props => {
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+    //console.log(classes)
+
+    return (
+        <button 
+            //evento: ao clicar receber o valor e passar para o display
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+            {props.label}
+            
+        </button>
+
+    )
+}
+
+//{console.log(typeof(classes))} teste acima /\
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+// export default props =>
+//     //uma forma...
+//     <button className={`
+//         button
+//         ${props.operation ? 'operation' : ''}
+//         ${props.double ? 'double' : ''}
+//         ${props.triple ? 'triple' : ''}
+//     `}>
+//         {props.label}
+//     </button>
