26cc1e8f4b309336dced44e57947333fdbf4ee41
lrb7dev <luanrb.dev@gmail.com>
2022-10-26 19:54:48 -0300

    new

--- sessao 22 - React/crud/frontend/src/components/template/Nav.jsx
+++ sessao 22 - React/crud/frontend/src/components/template/Nav.jsx
@@ -4,5 +4,13 @@ import React from 'react'
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props =>
     <aside className="menu-area">
-        Navegação
+        {/*TODO: REFATORAR EM CASA*/}
+        <nav className="menu">
+            <a href="#/">
+                <i className="fa fa-home"></i> Início
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"></i> Usuários
+            </a>
+        </nav>
     </aside>
