c78102de551d33dfbc1226c08b526154f881c6b5
lrb7dev <luanrb.dev@gmail.com>
2022-10-26 19:46:05 -0300

    new

--- sessao 22 - React/crud/frontend/src/components/template/Footer.jsx
+++ sessao 22 - React/crud/frontend/src/components/template/Footer.jsx
@@ -4,5 +4,6 @@ import React from 'react'
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props =>
     <footer className="footer">
-        Rodape
+        Desenvolvido com <i className="fa fa-heart text-danger"></i> por 
+        <strong> Cod<span className="text-danger">3</span>r</strong>
     </footer>
